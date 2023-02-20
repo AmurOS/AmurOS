2094ce1a335d1352f875b9c4fa7e4023cbd4a242
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-02-20 12:43:37 +0300

    #0.0.4f - changed location of multiboot header

--- WINDOWS_MAKEFILE.bat
+++ WINDOWS_MAKEFILE.bat
@@ -1,6 +1,8 @@
 @echo off
 @mkdir "build"
 @pause
+@nasm -f elf "src\boot\multiboot.asm" -o multiboot.o
+@move multiboot.o "build"
 @nasm -f elf32 "src\boot\boot.asm" -o boot.o
 @move boot.o "build"
 @nasm -f elf32 "src\boot\lib.asm" -o asmlib.o
@@ -9,6 +11,7 @@
 @move kernelc.o "build"
 @copy link.ld "build"
 @cd "build"
-@ld -T link.ld -m i386pe -o kernel boot.o asmlib.o kernelc.o -build-id=none
+@ld -T link.ld -m i386pe -o kernel multiboot.o boot.o asmlib.o kernelc.o -build-id=none
 @objcopy -O elf32-i386 kernel kernel.elf
 @del link.ld
+@pause

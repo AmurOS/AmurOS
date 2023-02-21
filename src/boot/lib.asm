36d69e85245bee5a02f6c00658f233c827a4313e
FrostySF <114286463+FrostySF@users.noreply.github.com>
2023-02-21 22:34:30 +0700

    0.0.5f - cursor edit

--- src/boot/lib.asm
+++ src/boot/lib.asm
@@ -7,8 +7,6 @@ global _write_port
 global _load_idt
 global _reboot
 global _videomode
-global _outb
-
 extern ___driver_kb_keyboard_handler_main
 
 _read_port:
@@ -49,12 +47,7 @@ _videomode:
 	mov cx,160
 	mov dx,100
 	mov al,4
-	
-_outb:
-    mov al, [esp + 8]    ; move the data to be sent into the al register
-    mov dx, [esp + 4]    ; move the address of the I/O port into the dx register
-    out dx, al           ; send the data to the I/O port
-    ret   
+
 section .bss
 resb 8192; 8KB for stack
 stack_space:
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -44,9 +44,8 @@ void kmain(void)
 
     __std__gotoxy(0, 0);
     __std__cls();
-    __std__printc(HEADER,112);
+    __std__printc(HEADER, 112);
     __std__gotoxy(0, 1);
-
     __std__printf("\nWelcome to AmurOS!\n");
     for (int i = 0;; i++)
     {
@@ -55,8 +54,8 @@ void kmain(void)
         __std__scanf(str);
         if (__std__strcmp(str, "help") == true)
         {
-            __std__printc("\n\n>-----help list-----<",112);
-            __std__printf("\n= reboot  cls       =\n= sleep   videomode =\n= test    echo      =\n= sysver            =\n=====================");
+            __std__printc("\n\n>-----help list-----<", 112);
+            __std__printf("\n= reboot  cls       =\n= sleep   videomode =\n= test    echo      =\n= sysver  setcursor =\n= beep              =\n=====================");
         }
         else if (__std__strcmp(str, "reboot") == true)
         {
@@ -66,14 +65,15 @@ void kmain(void)
         {
             __std__gotoxy(0, 0);
             __std__cls();
-            __std__printc(HEADER,112);
+            __std__printc(HEADER, 112);
             __std__gotoxy(0, 1);
         }
         else if (__std__strcmp(str, "sleep") == true)
         {
             __std__sleep(0x10FFFFFF);
         }
-        else if (__std__strcmp(str,"sysver")){
+        else if (__std__strcmp(str, "sysver"))
+        {
             __std__printf(SYSVER);
         }
         else if (__std__strcmp(str, "videomode") == true)
@@ -88,7 +88,7 @@ void kmain(void)
         {
             char *firstWord, *otherString;
             int WordLen = 0, otherStringLen = 0;
-            
+
             char *space = __std__strstr(str, " ");
             WordLen = space - str;
             otherStringLen = __std__strlen(str) - WordLen - 1;
@@ -96,12 +96,35 @@ void kmain(void)
             __std__newline();
             __std__printf(__std__strncpy(otherString, &space[1], otherStringLen + 1));
         }
+        else if (__std__strstr(str, "setcursor "))
+        {
+            char *firstWord, *otherString;
+            int WordLen = 0, otherStringLen = 0;
+
+            char *space = __std__strstr(str, " ");
+            WordLen = space - str;
+            otherStringLen = __std__strlen(str) - WordLen - 1;
+
+            if (__std__strcmp(__std__strncpy(otherString, &space[1], otherStringLen + 1), "false"))
+            {
+                write_port(COMMAND_PORT, 0x0A);
+                write_port(DATA_PORT, 0x20);
+            }
+            else{
+                write_port(COMMAND_PORT, 0x0A);
+                write_port(DATA_PORT, (read_port(DATA_PORT) & 0xC0) | 0);
+                write_port(COMMAND_PORT, 0x0A);
+                write_port(DATA_PORT, (read_port(DATA_PORT) & 0xC0) | 14);
+            }
+        }
+
+        
         __std__newline();
         if (__std__cursory > LINES - 1)
         {
             __std__gotoxy(0, 0);
             __std__cls();
-            __std__printc(HEADER,112);
+            __std__printc(HEADER, 112);
             __std__gotoxy(0, 1);
         }
     }

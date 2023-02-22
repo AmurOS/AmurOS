a24ae3193b7872169321c6b76b7ad8f9b66e4333
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-02-22 19:47:12 +0300

    #0.0.6 - changed NULL value

--- src/kernel/include.h
+++ src/kernel/include.h
@@ -1,17 +1,68 @@
-#include "lib.h"
-char *__driver_kb_kbbuffer = "";
-bool __driver_kb_endinp = false;
-int __driver_kb_kbbcur = 0;
-int __std__cursorx = 0;
-int __std__cursory = 0;
-void keyboard_handler(void);
-char read_port(bit8 port);
-void write_port(bit8 port, byte data);
-void load_idt(byte32 *idt_ptr);
-void reboot(void);
-void videomode(void);
-#include "colors.h"
-static void *__std__malloc(int sz);
-void __std__putc(char ch);
-#include "drivers/keyboard.h"
-#include "std.h"
+#define NULL '\0'
+#define new(t) malloc(sizeof(t))
+#define bool int
+#define true 1
+#define false 0
+#define bit8 unsigned short int
+#define byte unsigned char
+#define byte32 unsigned long
+#define byte32i unsigned int
+#define byte64 unsigned long long
+
+#define LINES 25
+#define COLUMNS_IN_LINE 80
+#define BYTES_FOR_EACH_ELEMENT 2
+#define SCREENSIZE BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
+
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+#define IDT_SIZE 256
+#define INTERRUPT_GATE 0x8e
+#define KERNEL_CODE_SEGMENT_OFFSET 0x08
+
+#define COMMAND_PORT 0x3D4
+#define DATA_PORT 0x3D5
+#define HIGH_BYTE_COMMAND 14
+#define LOW_BYTE_COMMAND 15
+
+#define ENTER_KEY_CODE 0x1C
+
+byte keyboard_map[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+  '9', '0', '-', '=', 0,	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};

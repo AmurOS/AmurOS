b2695db56ca19cc198ffc098a32277da006b6b8a
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-02-23 17:28:49 +0300

    #0.0.6e - more fixes

--- src/kernel/drivers/sound.h
+++ src/kernel/drivers/sound.h
@@ -1,25 +1,23 @@
 static void __driver_audio_play_sound(int nFrequence)
 {
-    int Div;
-    int tmp;
+    int __driver_audio_Div;
+    int __driver_audio_tmp;
 
-    Div = 1193180 / nFrequence;
+    __driver_audio_Div = 1193180 / nFrequence;
     write_port(0x43, 0xb6);
-    write_port(0x42, (int)(Div));
-    write_port(0x42, (int)(Div >> 8));
+    write_port(0x42, (int)(__driver_audio_Div));
+    write_port(0x42, (int)(__driver_audio_Div >> 8));
 
-    tmp = read_port(0x61);
-    if (tmp != (tmp | 3))
-    {
-        write_port(0x61, tmp | 3);
+    __driver_audio_tmp = read_port(0x61);
+    if (__driver_audio_tmp != (__driver_audio_tmp | 3)) {
+        write_port(0x61, __driver_audio_tmp | 3);
     }
 }
 
 static void __driver_audio_nosound()
 {
-    int tmp = read_port(0x61) & 0xFC;
-
-    write_port(0x61, tmp);
+    int __driver_audio_tmp2 = read_port(0x61) & 0xFC;
+    write_port(0x61, __driver_audio_tmp2);
 }
 
 void __driver_audio_beeps()
@@ -29,42 +27,9 @@ void __driver_audio_beeps()
     __driver_audio_nosound();
 }
 
-void __driver_audio_tone(int hz, int time)
+void __driver_audio_tone(int __driver_audio_hz, int __driver_audio_time)
 {
-    __driver_audio_play_sound(hz);
-    __std__sleep(time);
+    __driver_audio_play_sound(__driver_audio_hz);
+    __std__sleep(__driver_audio_time);
     __driver_audio_nosound();
-}
-
-// В лесу родилась Ёлочка
-void __driver_audio_music(){
-    __driver_audio_tone(247, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(370, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(329, 0x10FFFFFF);
-    __driver_audio_tone(247, 0x10FFFFFF);
-    __driver_audio_tone(247, 0x10FFFFFF);
-    __driver_audio_tone(247, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(370, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(497, 0x10FFFFFF);
-    __std__sleep(0x15FFFFFF);
-    __driver_audio_tone(497, 0x10FFFFFF);
-    __driver_audio_tone(277, 0x10FFFFFF);
-    __driver_audio_tone(2770, 0x10FFFFFF);
-    __driver_audio_tone(440, 0x10FFFFFF);
-    __driver_audio_tone(440, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(370, 0x10FFFFFF);
-    __driver_audio_tone(329, 0x10FFFFFF);
-    __driver_audio_tone(247, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(370, 0x10FFFFFF);
-    __driver_audio_tone(417, 0x10FFFFFF);
-    __driver_audio_tone(329, 0x10FFFFFF); 
 }

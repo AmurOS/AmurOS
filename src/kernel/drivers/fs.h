0f49c5555b839f35e764a9551d5372b106381002
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-05-22 11:43:03 +0300

    0.0.7n1

--- src/kernel/drivers/fs.h
+++ src/kernel/drivers/fs.h
@@ -1,16 +1,26 @@
 #define MEGABYTE(n) n*1048576
 
-volatile int __fs_fpointer = 0;
-volatile char *__fs_ramdisk = "";
+static int __fs_fpointer = 1;
+static char *__fs_ramdisk = "";
 dict_t **__fs_files;
+
+char *malloc_ptr;
+void *newmalloc(int size)
+{
+  void *ret;
+  ret = (void*)malloc_ptr;
+  malloc_ptr += size;
+  return ret;
+}
 
 void __fs_initdisk() {
 	__fs_files = dictAlloc();
-	__fs_ramdisk = (char *)__std__malloc(MEGABYTE(256));
+	__fs_ramdisk = (char *)newmalloc(MEGABYTE(256));
+	__fs_ramdisk[0] = 0;
 }
 
 char *__fs_reads(int start, int size) {
-	char *mem = (char*)__std__malloc(size);
+	char *mem = (char*)newmalloc(size);
 	for (int i = 0; i < size; i++) {
 		mem[i] = __fs_ramdisk[start+i];
 	}
@@ -31,7 +41,7 @@ void __fs_newfile(char *name, int size, byte *mem) {
 }
 
 void __fs_writefile(char *name, int size, byte *mem) {
-	if (getItem(__fs_files, name) == NULL) {
+	if (getItem(__fs_files, name) == -1) {
 		__fs_newfile(name, size, mem);
 	}
 	byte32i usize = (unsigned)size;
@@ -42,6 +52,8 @@ void __fs_writefile(char *name, int size, byte *mem) {
 
 char *__fs_readfile(char *name) {
 	int val = (int)getItem(__fs_files, name);
+	//if (val == 0)
+	//	return "Error";
 	byte *size = __fs_reads(val, 4);
 	byte *nsize = btarrev(size);
 	return __fs_reads(val+4, b8ati(size));

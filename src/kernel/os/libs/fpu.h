21cab1f40cab5ec8b9d3cea00c0e705c9385dfba
bas1c <68533770+bas1c1@users.noreply.github.com>
2023-10-26 19:52:33 +0300

    Delete src directory

--- src/kernel/os/libs/fpu.h
+++
@@ -1,2 +0,0 @@
-extern void fpu_set_control_word(const bit8 cw);
-extern void fpu_enable();
--- src/kernel/os/libs/idt.h
+++
@@ -1,5 +0,0 @@
-#define NO_IDT_DESCRIPTORS     256
-
-extern void idt_set_entry(int index, byte32i base, bit8 seg_sel, byte flags);
-
-extern void idt_init();
